4cf1d8b4d65bcb731c00e99e950b2e626ee475d1
Tarak Sonawane <43270421+TarakSonawane@users.noreply.github.com>
2025-03-16 02:39:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -170,5 +170,5 @@ def search_products():
 
 # ✅ Run the application
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
